7de0bb8f0f3323c6a6f7f6146fa1dc5146d7d7bf
karen queiroz <kqueiroz812@gmail.com>
2019-07-18 17:48:50 -0300

    adicionar atividade de calculo de media em js

---
+++ script.js
@@ -0,0 +1,35 @@
+let nome = prompt("Digite seu nome");
+ let dataDeNascimento= Number(prompt("Digite seu ano de nascimento"));
+ let ano = 2019;
+
+idade = ano - dataDeNascimento
+
+ console.log("Olá,eu me chamo " + nome + ",tenho " + idade + " anos e estou estudando JavaScript");
+
+
+
+ let nomedoaluno = prompt("Digite seu nome");
+ let matricula = Number (prompt("Digite seu numero de matricula"));
+ let nota1 = Number (prompt("Digite sua nota 1")); 
+ let nota2 = Number (prompt("Digite sua nota 2"));
+
+ let media = (nota1 = nota2)/2;
+  
+ if(media >0 && media  <10)
+        alert("O aluno(a) "+ nomedoaluno+ " matricula " + "obteve media final " + media);
+
+  
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
